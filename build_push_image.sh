5f1a0476270c002499ba06751ff8e3f6058fabba
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-02-26 23:28:36 +0700

    add ghcr build_push_image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# Melakukan build menggunakan Dockerfile
+# Melakukan build menjadi docker image
 docker build -t item-app:v1 .
 
 # Cek daftar docker images yang tersedia
 docker images
 
-# Push docker images ke Docker Hub
-cat ../pass.txt | docker login --username kixanax13 --password-stdin
-docker tag item-app:v1 kixanax13/item-app:v1
-docker push kixanax13/item-app:v1
+# Mengubah nama image sesuai dengan format Github Packages
+docker tag item-app:v1 ghcr.io/bancarafarros/item-app:v1
+
+# 4. Login ke GitHub Packages via Terminal
+cat ../pass.txt | docker login ghcr.io --username bancarafarros --password-stdin
+
+# Upload image ke Github Packages
+docker push ghcr.io/bancarafarros/item-app:v1
