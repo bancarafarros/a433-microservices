77f53a1926bf2335d324d9074b1e26c6a41dd76c
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-03-17 11:15:31 +0700

    add build_push_image_order_service.sh

---
+++ build_push_image_order_service.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Melakukan build menjadi docker image
+docker build -t order-service:v1 .
+# Cek daftar docker images yang tersedia
+docker images
+# Mengubah nama image sesuai dengan format Github Packages
+docker tag item-app:v1 ghcr.io/bancarafarros/order-service:v1
+# Login ke GitHub Packages via Terminal
+cat ../pass.txt | docker login ghcr.io --username bancarafarros --password-stdin
+# Upload image ke Github Packages
+docker push ghcr.io/bancarafarros/order-service:v1
