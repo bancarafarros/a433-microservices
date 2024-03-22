6c1dede38c2cbb4495264cc186d2dc42571f194c
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2024-03-22 19:18:09 +0700

    build_push_image_shipping_service.sh

--- build_push_image_shipping_service.sh
+++ build_push_image_shipping_service.sh
@@ -4,7 +4,7 @@ docker build -t shipping-service:v1 .
 # Cek daftar docker images yang tersedia
 docker images
 # Mengubah nama image sesuai dengan format Github Packages
-docker tag item-app:v1 ghcr.io/bancarafarros/shipping-service:v1
+docker tag shipping-service:v1 ghcr.io/bancarafarros/shipping-service:v1
 # Login ke GitHub Packages via Terminal
 cat ../pass.txt | docker login ghcr.io --username bancarafarros --password-stdin
 # Upload image ke Github Packages
